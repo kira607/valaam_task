0eb5d6a9aacdeac2d2df5c3962b08fc4c9d7e8e9
kira607 <kirill.lesckin@gmail.com>
2020-03-19 13:27:26 +0300

    New class Unit (without error msg'es)

--- main.cpp
+++ main.cpp
@@ -5,25 +5,215 @@
 #include <cstring>  //strcmp
 #include <functional> //hash
 
-namespace colors
+/*
+enum class colors
 {
-    char red[]  = "\033[31m";
-    char blue[] = "\033[34m";
-    char none[] = "\033[0m";
+    red,
+    blue,
+    none,
+};
+
+std::ostream& operator<< (std::ostream &out, colors col)
+{
+    switch(col)
+    {
+        case colors::red  :  out << "\033[31m";
+        case colors::blue :  out << "\033[34m";
+        case colors::none :  out << "\033[0m";
+        default          :  out.setstate(std::ios_base::failbit);
+    }
+    return out;
+}
+ */
+
+namespace color
+{
+    char  red []= "\033[31m";
+    char  blue[] = "\033[34m";
+    char  none[] = "\033[0m";
 }
 
-bool test_mode = false;
+constexpr bool test_mode = true;
+
+class Unit
+{
+private:
+    std::basic_string<char> unit;
+    int unit_size;
+    std::hash<std::basic_string<char>> hash_gen;
+    size_t unit_hash;
+
+    void init(unsigned int unit_size_in)
+    {
+        if(test_mode) printf("INIT, %d\n",unit_size_in);
+        try
+        {
+            if(unit_size_in>0)
+                unit.resize(unit_size_in,0);
+            else throw std::exception(); //! set def unit size
+        }
+        catch(const std::exception &ex) // catch std::bad_alloc
+        {
+            std::cout << color::red << "Error: " << ex.what() << color::none << "\n";
+            unit_size = 1024*1024;
+            unit.resize(unit_size,0);
+        }
+    }
+public:
+    explicit Unit(int unit_size_in = 0):unit_size{unit_size_in},unit_hash{0}
+    {
+        init(unit_size_in);
+    }
+    explicit Unit(char *unit_size_str):unit_hash{0}
+    {
+        if(test_mode) printf("Constructor CHAR*, size:%s\n",unit_size_str);
+        try
+        {
+            std::stringstream convert(unit_size_str);
+            if (!(convert >> unit_size))
+            {
+                std::cout << color::red << "Bad unit size. Setting default... (1MB)\n" << color::none;
+                unit_size = 1024*1024;
+            }
+            else
+            {
+                if(test_mode) printf("Convert succeed!\n");
+                if(unit_size <= 0)
+                    throw std::exception();
+                //std::cout << "Unit size: " << unit_size << " byte\n";
+            }
+        }
+        catch(const std::exception &ex)
+        {
+            if(strcmp(ex.what(),"") == 0)
+                std::cout << color::blue << "Did not receive unit size. Setting default... (1MB)\n" << color::none;
+            else
+                std::cout << color::red << "Bad unit size. Setting default... (1MB)\n" << color::none;
+
+            unit_size = 1024*1024;
+        }
+        init(unit_size);
+    }
+    ~Unit()
+    {
+        if(test_mode) printf("Destructor INT\n");
+    }
+
+    [[nodiscard]] int size() const
+    {
+        return unit_size;
+    }
+    [[nodiscard]] size_t hash() const
+    {
+        return unit_hash;
+    }
+    [[nodiscard]] std::basic_string<char> data() const
+    {
+        return unit;
+    }
+
+    void resize(int to_resize)
+    {
+        try
+        {
+            if(unit.empty()||to_resize>0)
+            unit.resize(to_resize);
+            else throw std::exception();
+        }
+        catch(const std::exception &ex) // catch std::bad_alloc
+        {
+            if(strcmp(ex.what(),"") == 0)
+                std::cout << color::blue << "Error: bad resize" << color::none;
+            else
+                std::cout << color::red << "Error: " << ex.what() << color::none << "\n";
+        }
+    }
+    void fill()
+    {
+        if(test_mode) printf("Fill\n");
+        //!check unit, size
+        try
+        {
+            if (!unit.empty())
+                for (int k = unit.size(); k < unit_size; ++k)
+                    unit.at(k) = '\0';
+        }
+        catch(const std::exception& ex)
+        {
+            std::cout << "Error: " << ex.what() << "\n";
+        }
+    }
+    void gen()
+    {
+        if(test_mode) printf("GEN\n");
+
+        //!check unit
+        if(!unit.empty())
+        unit_hash = hash_gen(unit);
+    }
+
+    void read(std::ifstream &file)
+    {
+        if(test_mode) printf("read\n");
+        //!check size & file
+        try
+        {
+            if(!unit.empty()&&unit_size>0)
+            file.read((char*)&unit[0], sizeof(char) * unit_size);
+            else throw std::exception();
+        }
+        catch(const std::exception &ex)
+        {
+            std::cout << color::red << "Error: " << ex.what() << color::none << "\n";
+        }
+    }
+
+    void write(std::ofstream &file)
+    {
+        if(test_mode) printf("WRITE\n");
+
+        try
+        {
+            if(!unit.empty()&&unit_size>0)
+            {
+                file.write((char *) &unit[0], sizeof(char) * unit_size);
+                file << unit_hash;
+            }
+            else throw std::exception();
+        }
+        catch(const std::exception &ex)
+        {
+            std::cout << color::red << "Error: " << ex.what() << color::none << "\n";
+        }
+    }
+
+    void disp()
+    {
+        if(test_mode) printf("DISP\n");
+        std::cout << unit << "\n";
+    }
+
+    void info()
+    {
+        std::cout << color::blue << "INFO:\n" << color::none;
+        if(unit.empty()) printf("no data\n");
+        else             printf("has data\n");
+        std::cout << "Unit size is " << unit_size << "\n";
+        std::cout << color::blue << "------------\n" << color::none;
+    }
+};
 
 //requires minimum 3 arguments
 //maximum 4
 int main(int argc, char *argv[])
 {
+    std::cout << color::blue;
     //check num of args
     if(argc > 4 || argc < 3)
     {
-        std::cout << colors::red <<
+        std::cout << color::red <<
                   "Got " << argc << " arguments, but 3 or 4 needed\nTerminating..." <<
-                  colors::none << "\n";
+                  color::none << "\n";
         return 1;
     }
 
@@ -32,7 +222,7 @@ int main(int argc, char *argv[])
     //check diffs btw in & out
     if(file_in_path == file_out_path)
     {
-        std::cout << colors::red << "Error: Cannot write incoming file in itself" << colors::none << "\n";
+        std::cout << color::red << "Error: Cannot write incoming file in itself" << color::none << "\n";
         file_out_path = "out.f";
         std::cout << "File out: " << file_out_path << "\n";
     }
@@ -45,75 +235,39 @@ int main(int argc, char *argv[])
     int file_size = fin.tellg();
     fin.seekg(0,std::ios::beg);
 
+    Unit unit(argv[3]);
+    if(test_mode) unit.info();
+
+    int block_num = 1;
+    int unit_num = file_size / unit.size();
+    if(file_size % unit.size() > 0) ++unit_num;
+
     std::cout << "size of in file: " << file_size << " byte\n";
-
-    //check & set unit size
-    int unit_size{};
-    try
-    {
-        std::stringstream convert(argv[3]);
-        if (!(convert >> unit_size))
-        {
-            std::cout << colors::red << "Bad unit size. Setting default... (1MB)\n" << colors::none;
-            unit_size = 1024*1024;
-        }
-        else
-        {
-            if(unit_size <= 0)
-                throw std::exception();
-            std::cout << "Unit size: " << unit_size << " byte\n";
-        }
-    }
-    catch(const std::exception &ex)
-    {
-        if(strcmp(ex.what(),"") == 0)
-            std::cout << colors::blue << "Did not receive unit size. Setting default... (1MB)\n" << colors::none;
-        else
-            std::cout << colors::red << "Bad unit size. Setting default... (1MB)\n" << colors::none;
-
-        unit_size = 1024*1024;
-    }
-
-    char unit[unit_size];
-
-    int block_num = 1,
-    unit_num = file_size / unit_size;
-    if(file_size % unit_size > 0) ++unit_num;
-
-    std::cout << "Units number: " << colors::blue << unit_num << colors::none << "\n";
+    std::cout << "Units number: " << color::blue << unit_num << color::none << "\n";
 
     //read -> write
     while(unit_num > 0)
     {
         //fill last unit
         if(unit_num == 1)
-        {
-            for(int k = 0; k < unit_size; ++k)
-                unit[k] = '\0';
-        }
+            unit.fill();
 
-        //read
-        fin.read((char*)unit, sizeof(char) * unit_size);
-
+        if(test_mode) unit.info();
+        unit.read(fin);
         // in
         if (test_mode)
         {
             std::cout << "----------------in " << block_num << "-----------------" << std::endl;
-            for (int i = 0; i < unit_size; ++i)
-            {
-                std::cout << unit[i];
-            }
-            std::cout << std::endl;
+            unit.disp();
+            printf("\n");
         }
 
         //generate hash
-        std::hash<std::string> hash_gen;
-        size_t hash = hash_gen(unit);
-        std::cout << colors::red << "HASH: " << colors::blue << hash << colors::none << std::endl;
+        unit.gen();
+        std::cout << "[" << block_num << "]" << color::red << "HASH: " << color::blue << unit.hash() << color::none << std::endl;
 
         //write
-        fout.write((char*)unit, sizeof(char) * unit_size);
-        fout << hash;
+        unit.write(fout);
         --unit_num;
         ++block_num;
     }
@@ -126,7 +280,8 @@ int main(int argc, char *argv[])
     {
         std::cout << "\n\n\n";
         std::cout << "----------------out-----------------" << std::endl;
-        system("cat file2.txt");
+        std::string to_cat = "cat " + file_out_path;
+        system(to_cat.c_str());
     }
 
     return 0;
