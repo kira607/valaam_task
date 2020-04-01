e91e81ca04325694ca97c6a35b2ff0077121fb1b
Kirill.Leskin <Kirill.Leskin@partner.bmw.de>
2020-04-01 15:01:32 +0300

    Added condit vars in header

--- condit_vars.h
+++ condit_vars.h
@@ -5,4 +5,9 @@
 #ifndef UNTITLED_CONDIT_VARS_H
 #define UNTITLED_CONDIT_VARS_H
 
+#include <condition_variable>
+
+static std::condition_variable cv_producer;
+static std::condition_variable cv_consumer;
+
 #endif //UNTITLED_CONDIT_VARS_H
